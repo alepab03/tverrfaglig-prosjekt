1aade5f12aaeffbc57b01934ff54c919b8508b6f
alepab03 <alepab03@innlandetfylke.no>
2026-03-13 11:43:00 +0100

    add button component

---
+++ nettside/src/components/button.tsx
@@ -0,0 +1,20 @@
+
+export default function Button(props: any) {
+
+    let btnColor;
+    if (props.color === 0) {
+        btnColor = "bg-(--green) hover:bg-[#00302B]"
+    } else if (props.color === 1) {
+        btnColor = "bg-(--light-green) hover:bg-[#005D2F]"
+    } /* usikker på om vi noen gang kommer til å bruke lysegrønn, men har laget det til en mulighet for nå */
+
+    return(
+        <div>
+            <a href={props.url}>
+                <button onClick={props.onClick} className={`pl-8 pr-8 pt-2.5 pb-2.5 ${btnColor} rounded-[0.313rem] cursor-pointer`}> {/* Er det mulig å gjøre sånn og omitte når man ikke trenger?? jeg vet ikke */}
+                    <p className="font-bold text-white">{props.title}</p>
+                </button>
+            </a>
+        </div>
+    );
+}
