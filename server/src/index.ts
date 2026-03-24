f5d0e0ecf45a718fff144ac2fe2b7f567e3a3ee0
alepab03 <alepab03@innlandetfylke.no>
2026-03-24 10:16:00 +0100

    add route path? something like that

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import authRouter from './routes/authRoutes.ts';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/auth', authRouter);
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running!")
